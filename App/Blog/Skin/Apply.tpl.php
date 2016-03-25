1d052642ef042658c7c20f6458ae899c4234756b
hongbo819 <hongbo819@163.com>
2016-03-25 14:24:33 +0800

    apply page modify

--- App/Blog/Skin/Apply.tpl.php
+++ App/Blog/Skin/Apply.tpl.php
@@ -10,53 +10,44 @@
         <div class="content">
             <div class="content_resize">
                 <div class="mainbar">
-                    <div class="article">
-                        <h2>
-                            录入网站信息
-                        </h2>
+                        <h1>录入网站信息</h1><h3>标 <m>*</m> 为必填内容</h3>
                         <form action="" method="post">
                             <ol>
                                 <li>
-                                    <label>网站昵称:</label> <input type="text" name="webName" class="input_middle">
+                                    <label><m>*</m>网站昵称:</label> <input type="text" name="webName" class="input_middle">
                                 </li>
                                 <li>
-                                    <label>一句话介绍网站:</label> <input type="text" name="webDesc" class="input_middle">
+                                    <label><m>*</m>一句话介绍网站:</label> <input type="text" name="webDesc" class="input_middle">
                                 </li>
                                 <li>
-                                    <label>网站二级地址（如hello.cuihongbo.com,只需填写hello即可，不可修改）:</label> <input type="text" name="webSite" class="input_middle">
+                                    <label><m>*</m>网站二级地址（如hello.zhbor.com,只需填写hello即可，不可修改）:</label> <input type="text" name="webSite" class="input_middle">
                                 </li>
                                 <li>
-                                    <label>网站分类（建议4-10个分类，用英文逗号,分割）:</label> <input type="text" name="cate" class="input_middle">
+                                    <label><m>*</m>网站分类（建议4-10个分类，用英文逗号,分割）:</label> <input type="text" name="cate" class="input_middle">
                                 </li>
                                 <li>
-                                    <label>管理员邮箱:</label> <input type="password" name="email" class="input_middle">
+                                    <label><m>*</m>管理员邮箱:</label> <input type="password" name="email" class="input_middle">
                                 </li>
                                 <li>
-                                    <label>管理员账号:</label> <input type="text" name="adminName" class="input_middle">
+                                    <label><m>*</m>管理员账号:</label> <input type="text" name="adminName" class="input_middle">
                                 </li>
                                 <li>
-                                    <label>管理员密码:</label> <input type="password" name="adminPassword" class="input_middle">
+                                    <label><m>*</m>管理员密码:</label> <input type="password" name="adminPassword" class="input_middle">
                                 </li>
                                 <li>
                                     <label>微博id（用于页面显示自己微博关注按钮，可不填）:</label> <input type="text" name="weiboUid" class="input_middle">
                                 </li>
                                 <li>
-                                    <label><input name="submit" type="submit" value="提交信息"></label>
+                                    <br/><input name="submit" type="submit" value="提交信息">
                                 </li>
                             </ol>
                         </form>
-                    </div>
                 </div>
+            <div class="myfoot">
+                Copyright © http://www.zhbor.com
+            </div>
             </div>
         </div>
-        <div class="fbg">
-            <div class="footer">
-                <div class="footer_resize">
-                    <p class="lf">
-                        Copyright © http://www.zhbor.com
-                    </p>
-                </div>
-            </div>
         </div>
         <script>
 			var isError = "<?=$error?>";
