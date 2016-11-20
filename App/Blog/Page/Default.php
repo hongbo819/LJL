c666b91a22d5309b1250c2c2e55dd11ecd1fb7b1
hongbo819 <hongbo819@163.com>
2016-11-20 11:05:34 +0800

    取消导图

--- App/Blog/Page/Default.php
+++ App/Blog/Page/Default.php
@@ -54,7 +54,7 @@ class Blog_Page_Default extends Blog_Page_Abstract
         
         //首页展示图片
         $showImgs = '';
-        $imgStrFile = '/tmp/'.APP_BLOG_NAME.'.php';
+        $imgStrFile = '';//'/tmp/'.APP_BLOG_NAME.'.php';
         if(file_exists($imgStrFile) && $page < 2) {
             $imgStr = file_get_contents($imgStrFile);
             $imgArr = json_decode($imgStr);
